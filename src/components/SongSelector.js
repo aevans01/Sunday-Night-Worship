de27ca4753d36647c9ec1129c23d2ea5ae661e50
Austin Evans <70028812+aevans01@users.noreply.github.com>
2024-02-26 19:29:39 -0600

    cleaned up commented code & added some css

--- src/components/SongSelector.js
+++ src/components/SongSelector.js
@@ -2,24 +2,6 @@ import React, { useEffect, useState } from "react";
 import CustomWheel from "./CustomWheel";
 
 function SongSelector() {
-    
-    
-
-    
-
-    
-    
-    function chooseRandomSong() {
-        /*var length = List.length;
-        var random = Math.random();
-        console.log("length: " + length);
-        console.log("Random Num: " + random);
-        console.log("List item: " + List[0]);
-        return List[Math.floor(length * random)]*/
-    }
-
-
-
     return (
         <CustomWheel
             wheelData = {[{option: "test"}]}/>
