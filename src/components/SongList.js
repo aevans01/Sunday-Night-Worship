e5b695ab47d94994d48e5cc9ae20eeeb58cc47b3
Austin Evans <70028812+aevans01@users.noreply.github.com>
2026-08-23 17:25:37 -0500

    Update SongList.js

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,40 +4,51 @@ import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 
 
-function SongList(){
-const [songs,setSongs] = useState();
+function SongList() {
+    const [songs, setSongs] = useState();
 
-useEffect(() => {
-    Axios.get(`https://hhbc-snw-api.netlify.app/api/getSongs`)
-        .then((response) => {
-            console.log("test")
-            if (response.data) {
-                setSongs(response.data)
-                console.log(response.data);
+    useEffect(() => {
+        Axios.get(`https://hhbc-snw-api.netlify.app/api/getSongs`)
+            .then((response) => {
+                console.log("test")
+                if (response.data) {
+                    setSongs(response.data)
+                    console.log(response.data);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
+
+    function deleteSong(id) {
+        Axios.delete(
+            'https://hhbc-snw-api.netlify.app/api/deleteSong',
+            {
+                data: { id }
             }
-        })
-        .catch(err => {
-            console.log(err);
-        });
-}, [songs]);
-
-    function deleteSong(songID){
-        Axios.delete()
+        )
+            .then(() => {
+                setSongs(songs.filter(song => song.id !== id));
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
-    return(
+    return (
         <>
-        <Table id='songListTable' striped bordered hover>
-        <tr key={"header"}>
-          <th></th>
-          <th>Title</th>
-      </tr>
-      {songs.map((item) => (
-        <tr key={item.id}>
-            <td><Button>Delete</Button></td>
-            <td>{item.VideoTitle}</td>
-        </tr>
-      ))}
-        </Table>
+            <Table id='songListTable' striped bordered hover>
+                <tr key={"header"}>
+                    <th></th>
+                    <th>Title</th>
+                </tr>
+                {songs.map((item) => (
+                    <tr key={item.id}>
+                        <td><Button onClick={() => deleteSong(item.id)}>Delete</Button></td>
+                        <td>{item.VideoTitle}</td>
+                    </tr>
+                ))}
+            </Table>
         </>
     )
 }
